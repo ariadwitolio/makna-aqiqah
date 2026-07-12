8c16ae5e9281538e58e65793a1bdc8a4bfe8c1fa
ariadwitolio <ariadwitolio@gmail.com>
2026-07-13 01:47:02 +0700

    add payload database migration

---
+++ apps/cms/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260712_184253 from './20260712_184253.js';
+
+export const migrations = [
+  {
+    up: migration_20260712_184253.up,
+    down: migration_20260712_184253.down,
+    name: '20260712_184253'
+  },
+];
